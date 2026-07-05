913dc8674f7b89af660111816846d515fb79daad
Rishit1606 <singhrishit1606@gmail.com>
2026-07-05 18:39:57 -0500

    load data, drop useless columns, fix age outlier and missing values

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
 import pandas as pd
-df = pd.read_csv('data/autism_screening.csv')
-print(df.shape)
-print(df.columns.tolist())
-print(df.head())
--- train.py
+++ train.py
@@ -6,3 +6,21 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report, accuracy_score
 from xgboost import XGBClassifier
 import pickle
+
+df = pd.read_csv('data/autism_screening.csv')
+df = df.drop(columns =['age_desc','result'])
+
+# replaces all values where conditions are not met with NaN
+df['age'] = df['age'].where(df['age'] <= 100)
+
+# filling NaN with median age
+df['age'] = df['age'].fillna(df['age'].median())
+
+
+
+print(df['age'].describe())
+print(df.shape)
+print(df.isnull().sum())
+
+# Shows how many YES vs NO in our target column
+print(df['Class/ASD'].value_counts())
